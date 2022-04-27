aa9da1a6dda90f306e962d506b3a331afc23b3c3
Nao-Sugahara <nao.sugahara@veriserve.co.jp>
2022-04-27 12:16:49 +0900

    Update test_action.py

--- test_action.py
+++ test_action.py
@@ -69,11 +69,11 @@ class TestHotelPlanisphere(object):
         ), "当日以前の日付を設定することができないこと"
 
         # 名前が空の状態では予約できないこと
-        ## 前日を確認する
+        ##前日を確認する
         d_today = datetime.today()
         d_tomorrow = d_today + timedelta(days=+1)
 
-        ## 日付を入力する
+        ##日付を入力する
         textbox = driver.find_element(By.ID, "date")
         textbox.clear()
         textbox.send_keys(d_tomorrow.strftime("%Y/%m/%d"))
@@ -117,7 +117,7 @@ class TestHotelPlanisphere(object):
         d_today = datetime.today()
         d_threemonth = d_today + timedelta(days=+91)
 
-        ##日付を入力する
+        ## 日付を入力する
         textbox = driver.find_element(By.ID, "date")
         textbox.clear()
         textbox.send_keys(d_threemonth.strftime("%Y/%m/%d"))
